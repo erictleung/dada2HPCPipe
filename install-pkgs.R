2bc300a221ff19d7fbe9f6254777a6801f1fd3de
Eric Leung <eric@erictleung.com>
2017-07-13 18:32:11 -0700

    Replace short ifelse with long form conditional
    The short hand if-else statement was not working, so this commit
    rewrites this in long form to make the conditional work correctly.

--- install-pkgs.R
+++ install-pkgs.R
@@ -3,14 +3,22 @@
 # Description:
 #   This script checks your R environment to be compatible with the DADA2
 #   pipeline.
+#
+#   Software versions:
+#       - R (>3.4.0)
+#       - Bioconductor (>3.5)
 
-# Check R version
+# Set minimum R version and find existing R version
 R_min_version <- "3.4.0"
 R_version <- paste0(R.Version()$major, ".", R.Version()$minor)
-ifelse(compareVersion(R_version, R_min_version) < 0,
-       message("R version looks okay:\n", version$major, ".", version$minor),
-       stop(paste("You need an R version >", R_min_version, "\n"))
 
-# Install Bioconductor, Version 3.5
+# Compare and give feedback on steps to take, given found R version
+if (compareVersion(R_version, R_min_version) <= 0) {
+    message("R version looks okay:\n", version$major, ".", version$minor)
+} else {
+    stop(paste("You need an R version >", R_min_version, "\n"))
+}
+
+# Install Bioconductor (Current Version 3.5)
 source("https://bioconductor.org/biocLite.R")
 biocLite()
