14aaf807cb5f7ebf42b3da4b862e93874f2bd76a
Eric Leung <eric@erictleung.com>
2017-07-30 22:36:40 -0700

    Fix version comparison
    This commit fixes essentially the sign of the version comparison on
    which parameter is the minimum R version that should be allowed.

--- install-pkgs.R
+++ install-pkgs.R
@@ -13,7 +13,7 @@ R_min_version <- "3.4.0"
 R_version <- paste0(R.Version()$major, ".", R.Version()$minor)
 
 # Compare and give feedback on steps to take, given found R version
-if (compareVersion(R_version, R_min_version) <= 0) {
+if (compareVersion(R_version, R_min_version) >= 0) {
     message("R version looks okay:\n", version$major, ".", version$minor)
 } else {
     stop(paste("You need an R version >", R_min_version, "\n"))
