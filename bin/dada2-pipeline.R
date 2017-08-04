3761e099b6196463d0781aaa98ad51818ec921e2
Eric Leung <eric@erictleung.com>
2017-08-04 14:41:41 -0700

    Start draft DADA2 workflow script

---
+++ bin/dada2-pipeline.R
@@ -0,0 +1,22 @@
+#!/usr/bin/env Rscript
+
+# Title:
+#   DADA2 Automated Workflow
+# Description:
+#   This workflow stems from the big data workflow tutorial listed in the DADA2
+#   documentation.
+#
+#   This workflow should be run automatically from the root directory of this
+#   repository using the Make utility.
+# Requirements:
+#   - R (>=3.4.0)
+#   - DADA2 (>=1.4.0)
+# Resources:
+#   - http://benjjneb.github.io/dada2/bigdata.html
+# Authored:
+#   Eric Leung (@erictleung)
+
+main() {
+
+}
+main()
