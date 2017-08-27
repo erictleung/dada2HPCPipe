3c1748a40bd11023093aef0b2f95ec6b0d2f7b63
Eric Leung <eric@erictleung.com>
2017-08-27 02:32:46 -0700

    Add references to package help page

--- R/dada2HPCPipe.R
+++ R/dada2HPCPipe.R
@@ -6,6 +6,15 @@
 #' break down the workflow into parts based on stopping points that require a
 #' user's input for the next steps.
 #'
+#' @references Callahan, Benjamin J., et al. "DADA2: high-resolution sample
+#'   inference from Illumina amplicon data." Nature Methods 13.7 (2016):
+#'   581-583. \url{http://dx.doi.org/10.1038/nmeth.3869}
+#' @references DADA2 GitHub page \url{https://github.com/benjjneb/dada2}
+#' @references Callahan, Ben J., et al. "Bioconductor workflow for microbiome
+#'   data analysis: from raw reads to community analyses." F1000Research 5
+#'   (2016). \url{http://dx.doi.org/10.12688/f1000research.8986.2}
+#' @references DADA2 Homepage \url{http://benjjneb.github.io/dada2/}
+#'
 #' @importFrom phyloseq sample_data
 #' @importFrom dplyr filter_
 #' @importFrom dada2 fastqPairedFilter
