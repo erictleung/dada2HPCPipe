019cf6b07e274e5cccf62608b2967030bfeac3ea
Eric Leung <eric@erictleung.com>
2017-08-30 08:39:36 -0700

    Add test SLURM script

---
+++ scripts/test_pipeline.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash -l
+
+# SBATCH --job-name=test_dada2_pipe
+# SBATCH --output=dada2_out.txt
+# SBATCH --error=error.txt
+# SBATCH --time=01:20:00
+# SBATCH --partition=exacloud
+# SBATCH --mail-type=END,FAIL
+# SBATCH --mail-user=user@univ.edu
+
+# Print out make commands
+cd ..; make help
