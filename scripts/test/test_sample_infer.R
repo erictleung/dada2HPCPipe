c632036994b6a91a700b08a8b32d7a908ae8a3e5
Eric Leung <eric@erictleung.com>
2017-09-20 18:58:55 -0700

    Create draft script to do sample inference step

---
+++ scripts/test/test_sample_infer.R
@@ -0,0 +1,25 @@
+# Load package
+cat("Loading dada2HPCPipe and other packages...\n")
+cat("Loading dada2HPCPipe package...\n")
+library(dada2HPCPipe); packageVersion("dada2HPCPipe")
+cat("Loading dada2 package...\n")
+library(dada2); packageVersion("dada2")
+cat("Loading phyloseq package...\n")
+library(phyloseq); packageVersion("phyloseq")
+
+# Variables
+temp_results <- "results"
+step_results_file <- file.path(temp_results, "dada2_post_filter_errors.RData")
+sample_infer_file <- file.path(temp_results, "dada2_post_sample_infer.RData")
+
+# Load data from previous step
+load(step_results_file)
+
+# Perform dereplication and denoising steps
+dada_result <- do_sample_infer_step(out$filtFs, errs$err_f,
+                                    out$filtRs, errs$err_r,
+                                    input$seq_names)
+
+# Save results of file
+cat("Saving results of dereplication and sample inference\n")
+save(dada_results, file = sample_infer_file)
