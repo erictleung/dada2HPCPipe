7f1e2cd8bbccd4cff50274ef5f2bbd2f8906a47c
Eric Leung <eric@erictleung.com>
2017-09-21 00:43:39 -0700

    Add taxonomy assignment SLURM scripts

---
+++ scripts/test/test_taxonomy.R
@@ -0,0 +1,21 @@
+# Load package
+cat("Loading dada2HPCPipe and other packages...\n")
+cat("Loading dada2HPCPipe package...\n")
+library(dada2HPCPipe); packageVersion("dada2HPCPipe"); cat("\n")
+
+# Variables
+temp_results <- "results"
+sample_infer_file <- file.path(temp_results, "dada2_post_sample_infer.RData")
+refdb <- "../../refs/rdp_train_set_16.fa.gz"
+seqresults <- file.path(temp_results, "sequence_table.rds")
+
+# Load data from inference step
+load(sample_infer_file)
+
+# Construct sequence table
+seqtab <- do_taxonomy_step(dada_result$mergers, ref_db = refdb)
+
+# Save sequence table
+cat("Writing out sequence table...\n")
+saveRDS(seqtab, file = seqresults)
+cat("Finish writing!\n")
---
+++ scripts/test/test_taxonomy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# SBATCH --job-name=test_taxonomy
+# SBATCH --output=dada2_out.txt
+# SBATCH --error=error.txt
+# SBATCH --time=01:20:00
+# SBATCH --partition=exacloud
+
+# Assign taxonomy for inferred results
+date
+time Rscript test_taxonomy.R
