dd8f06e076dcfcaab2425eb98accca16cdf14202
Eric Leung <eric@erictleung.com>
2017-09-25 01:29:27 -0700

    Add DADA2 tracking SLURM scripts

---
+++ scripts/test_5_track.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# SBATCH --job-name=test_track
+# SBATCH --output=dada2_out.txt
+# SBATCH --error=error.txt
+# SBATCH --time=01:20:00
+# SBATCH --partition=exacloud
+
+# Script variables
+SCRIPTNAME=test_5_taxonomy.sh
+RSCRIPT=src/test_track.R
+
+# Assign taxonomy for inferred results
+echo "Title: Track Reads Through Pipeline"
+echo "Script: ${SCRIPTNAME}"
+echo "R Script: ${RSCRIPT}"
+date
+echo ""
+time Rscript $RSCRIPT
